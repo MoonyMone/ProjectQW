83fc6ca12412f9492998e7a380d16bf4cac8d894
Moony <home.med09@gmail.com>
2023-03-15 03:45:28 +0300

    Preparing project.

--- ProjectQW/Views/AuthView.xaml.cs
+++ ProjectQW/Views/AuthView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ProjectQW
+namespace ProjectQW.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для AuthView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AuthView : Window
     {
-        public MainWindow()
+        public AuthView()
         {
             InitializeComponent();
         }
